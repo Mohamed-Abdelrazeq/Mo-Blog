6f5691a5acc1ed92fa48de9821e0d6b73fc7c76f
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2021-07-18 13:18:45 +0200

    Json Server Stable

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './Component/App';
-// import * as firebase from 'firebase';
+import firebase from 'firebase';
 
-// var config = {
-//   apiKey: "AIzaSyAq8TAHg5wsm-nvJ_Sz6W7sq6tq_X1mNeI",
-//   authDomain: "mo-blog-react.firebaseapp.com",
-//   projectId: "mo-blog-react",
-//   storageBucket: "mo-blog-react.appspot.com",
-//   messagingSenderId: "152506069079",
-//   appId: "1:152506069079:web:c8dfa9c7efde74a7947312"
-// };
+var config = {
+  apiKey: "AIzaSyAq8TAHg5wsm-nvJ_Sz6W7sq6tq_X1mNeI",
+  authDomain: "mo-blog-react.firebaseapp.com",
+  projectId: "mo-blog-react",
+  storageBucket: "mo-blog-react.appspot.com",
+  messagingSenderId: "152506069079",
+  appId: "1:152506069079:web:51343be78d927d76947312"
+};
 
-// firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 
 ReactDOM.render(
