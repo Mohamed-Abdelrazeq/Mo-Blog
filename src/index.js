a6ea8840337703cd7ad73438312c4d500aafe879
Mohamed-Abdelrazeq <62867599+Mohamed-Abdelrazeq@users.noreply.github.com>
2021-07-18 13:04:53 +0200

    Deployed using firebase

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './Styles/index.css';
 import App from './Component/App';
+// import * as firebase from 'firebase';
+
+// var config = {
+//   apiKey: "AIzaSyAq8TAHg5wsm-nvJ_Sz6W7sq6tq_X1mNeI",
+//   authDomain: "mo-blog-react.firebaseapp.com",
+//   projectId: "mo-blog-react",
+//   storageBucket: "mo-blog-react.appspot.com",
+//   messagingSenderId: "152506069079",
+//   appId: "1:152506069079:web:c8dfa9c7efde74a7947312"
+// };
+
+// firebase.initializeApp(config);
+
 
 ReactDOM.render(
   <React.StrictMode>
